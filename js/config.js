11815a2acf1c7f4e9e10fbbc976e1a7256f6f4f6
SBardak <sebouhbardakjian@gmail.com>
2025-08-04 22:29:57 -0700

    Fixed supabase urls

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 // Supabase configuration
 export const supabaseConfig = {
-    url: 'https://tdskwpcssbovburunekn.supabase.co',
-    key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRkc2t3cGNzc2JvdmJ1cnVuZWtuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQwMDI1MTUsImV4cCI6MjA2OTU3ODUxNX0.cFVJvrU2TorJMbfy8VpBG2T_pkSAMEoHgBqx-euig6M'
+    supabaseUrl: 'https://tdskwpcssbovburunekn.supabase.co',
+    supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRkc2t3cGNzc2JvdmJ1cnVuZWtuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQwMDI1MTUsImV4cCI6MjA2OTU3ODUxNX0.cFVJvrU2TorJMbfy8VpBG2T_pkSAMEoHgBqx-euig6M'
 };
